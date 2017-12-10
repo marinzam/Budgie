042247cc53791a99214deea1938c0efa3f127e7e
Kurtis Bass <kurtis.j.bass@gmail.com>
2017-12-10 15:39:27 -0500

    Init Commit for ModifyBudget.php and adding equality check into budget object

--- ModifyBudget.php
+++ ModifyBudget.php
@@ -1,13 +1,28 @@
 <?php
 session_start();
+$data = json_decode(file_get_contents('php://input'));
 include("AuxFunctions.php");
 $db = new mysqli("classroom.cs.unc.edu", "kjbass", "426password!", "kjbassdb");
 if(!isset($_SESSION['userID'])){
     header("Location: http://www.google.com/");
 }
 $userID = $_SESSION['userID']; 
+
+$budget = new Budget(NULL,NULL,NULL,NULL);
+$budget->constructJSON($data);
 $budgetID = getBudgetID($db, $userID);
+$savedBudget = getBudget($budgetID, $db);
 
 
-echo json_encode(getBudget($budgetID, $db));
+
+
+
+
+
+
+
+
+
+
+echo json_encode($budget->equals($savedBudget));
 ?>
