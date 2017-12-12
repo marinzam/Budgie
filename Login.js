c170bd3aa85a48314e8cff24a379b470321032c5
marinzam <marinzam@live.unc.edu>
2017-12-12 15:05:04 -0500

    now

--- Login.js
+++ Login.js
@@ -18,7 +18,7 @@ $(document).ready(function () {
             },
             error: function(response) {
                 console.log(response);
-                
+
             }});
     }); //login button push
 
